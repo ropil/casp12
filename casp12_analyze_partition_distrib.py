bfa8a718a4a7a2c70a739d78e32528654f86eec3
Robert <robert.pilstal@liu.se>
2017-07-13 16:27:10 +0200

    Finished the database creation

--- casp12_analyze_partition_distrib.py
+++ casp12_analyze_partition_distrib.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from re import compile
+from sqlite3 import connect
 
 '''
  Analyze domain partition distribution in target set, storing result in database
@@ -31,6 +32,49 @@ def get_version_str():
 
 
 # Library functions
+def create_database():
+    # Create in-memory
+    """Creates the database, in memory, to use for analyzing domain partitions
+
+    :return: the database connection handle
+    """
+
+    '''Copy the following to sqlite3 prompt to try it in console
+    CREATE TABLE casp(id int PRIMARY KEY);
+    CREATE TABLE target(id text, len int, casp int REFERENCES casp(id), PRIMARY KEY (id, casp));
+    CREATE TABLE domain(num int, target text REFERENCES target(id), casp int REFERENCES target(casp), PRIMARY KEY (num, target, casp));
+    CREATE TABLE segment(start int, stop int, len int, domain int REFERENCES domain(num), target text REFERENCES domain(target), casp int REFERENCES domain(casp), PRIMARY KEY (start, domain, target, casp));
+    CREATE TRIGGER segment_length_insert BEFORE INSERT ON segment FOR EACH ROW BEGIN UPDATE NEW SET len = stop - start + 1; END;
+    CREATE TRIGGER segment_length_update BEFORE UPDATE ON segment FOR EACH ROW BEGIN UPDATE NEW SET len = stop - start + 1; END;
+    CREATE VIEW domain_size (casp, target, domain, dlen, nseg) AS SELECT domain.casp, domain.target, domain.num, SUM(segment.len), COUNT(*) FROM domain INNER JOIN segment ON (domain.casp = segment.casp, domain.target = segment.target, domain.num = segment.domain) GROUP BY domain.casp, domain.target, domain.num ORDER BY dlen;
+    '''
+
+    database = connect(":memory:")
+    # CASP table
+    database.execute("CREATE TABLE casp(id int PRIMARY KEY);")
+    # CASP targets table
+    #
+    database.execute("CREATE TABLE target(id text, " +
+                     "len int, " +
+                     "casp int REFERENCES casp(id), " +
+                     "PRIMARY KEY (id, casp));")
+    # Corresponding domains table
+    # "CREATE TABLE domain(num int, target text REFERENCES target(id), casp int REFERENCES target(casp), PRIMARY KEY (num, target, casp));
+    database.execute("CREATE TABLE domain(num int, " +
+                     "target text REFERENCES target(id), " +
+                     "casp int REFERENCES target(casp), " +
+                     "PRIMARY KEY (num, target, casp));")
+    # Segment definitions table
+    database.execute("CREATE TABLE segment(start int, stop int, len int, domain int REFERENCES domain(num), target text REFERENCES domain(target), casp int REFERENCES domain(casp), PRIMARY KEY (start, domain, target, casp));")
+    # Synthetic variables
+    # Triggers for segment length calculation
+    database.execute("CREATE TRIGGER segment_length_insert BEFORE INSERT ON segment FOR EACH ROW BEGIN UPDATE NEW SET len = stop - start + 1; END;")
+    database.execute("CREATE TRIGGER segment_length_update BEFORE UPDATE ON segment FOR EACH ROW BEGIN UPDATE NEW SET len = stop - start + 1; END;")
+    # Domain size view, sorted by largest domain
+    database.execute("CREATE VIEW domain_size (casp, target, domain, dlen, nseg) AS SELECT domain.casp, domain.target, domain.num, SUM(segment.len), COUNT(*) FROM domain INNER JOIN segment ON (domain.casp = segment.casp, domain.target = segment.target, domain.num = segment.domain) GROUP BY domain.casp, domain.target, domain.num ORDER BY dlen;")
+    return database
+
+
 def read_domain(infile):
     """Parse the domain definitions and the corresponding segments in a given
     domain file.
