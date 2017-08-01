d069f98517a27f85da303d2dce4a4b8c646f1838
Robert <robert.pilstal@liu.se>
2017-08-01 15:32:16 +0200

    Documentation

--- files/pcons.py
+++ files/pcons.py
@@ -2,23 +2,35 @@ from os import path
 
 
 def pcons_domain_specifications(casp, target, database):
-    query = 'SELECT len FROM target WHERE casp="{}" AND id="{}";'.format(casp, target)
+    """Get domain ignore specifications for PCONS
+
+    :param casp: integer CASP experiment serial identifier
+    :param target: string target identifier
+    :param database: sqlite3 connector object to domain definition database
+    :return: dictionary with domain ID's as keys containing a full text PCONS
+             ignore-file definition
+    """
+    query = 'SELECT len FROM target WHERE casp="{}" AND id="{}";'.format(casp,
+                                                                         target)
     target_length = database.execute(query).fetchone()[0]
     ignore_residues = {}
 
     # Sum domain lengths if target length not specified
     if target_length is None:
-        query = "SELECT SUM(dlen) FROM domain_size WHERE casp={} AND target='{}' GROUP BY casp, target;".format(casp, target)
+        query = "SELECT SUM(dlen) FROM domain_size WHERE casp={} AND target='{}' GROUP BY casp, target;".format(
+            casp, target)
         target_length = database.execute(query).fetchone()[0]
 
     # For every domain
-    query = "SELECT num FROM domain WHERE casp={} AND target='{}';".format(casp, target)
+    query = "SELECT num FROM domain WHERE casp={} AND target='{}';".format(casp,
+                                                                           target)
     for (domain,) in database.execute(query):
         # Make an ignore-all template
         ignore_residues[domain] = set([i for i in range(1, target_length + 1)])
         # Collect all domain residues from all segments
         domain_residues = set()
-        query = "SELECT start, stop FROM segment WHERE casp={} AND target='{}' AND domain={};".format(casp, target, domain)
+        query = "SELECT start, stop FROM segment WHERE casp={} AND target='{}' AND domain={};".format(
+            casp, target, domain)
         for (start, stop) in database.execute(query):
             domain_residues = domain_residues.union(range(start, stop + 1))
         # Only ignore non-domain residues
