6caf4a11adb308de145e3bb9aa73d0c6e77cc431
Robert <robert.pilstal@liu.se>
2017-10-19 16:33:48 +0200

    Plotting scripts, scatter might not work

--- casp12_plot_scatter.py
+++ casp12_plot_scatter.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import sqlite3
+import seaborn as sns
+import matplotlib.pyplot as plt
 from casp12.plots import convert_data, d2S, get_correlates, plot_correlates
 
 
 '''
- Plot correlation data in quality assessment sqlite3 database
+ Plot scatter data in quality assessment sqlite3 database
  Copyright (C) 2017  Robert Pilstål
 
    This program is free software: you can redistribute it and/or modify
@@ -25,7 +27,7 @@ from casp12.plots import convert_data, d2S, get_correlates, plot_correlates
 # Version and license information
 def get_version_str():
     return "\n".join([
-        "casp12_plot_correlation  Copyright (C) 2017  Robert Pilstål;",
+        "casp12_plot_scatter  Copyright (C) 2017  Robert Pilstål;",
         "This program comes with ABSOLUTELY NO WARRANTY.",
         "This is free software, and you are welcome to redistribute it",
         "under certain conditions; see supplied General Public License."
@@ -33,6 +35,14 @@ def get_version_str():
 
 
 # Library functions
+def plot_scatter(correlates):
+    sns.set(style="ticks", color_codes=True)
+    #sns.set(style="ticks")
+    #f = sns.pairplot(correlates, hue="species")
+    #f = sns.pairplot(correlates, kind="reg", diag_kind="kde", plot_kws=dict(s=50, edgecolor="b", linewidth=1), diag_kws=dict(s=50, edgecolor="b", linewidth=1, shade=True))
+    f = sns.pairplot(correlates, kind="reg", diag_kind="kde",
+                     diag_kws=dict(shade=True), x_vars=["CASP12_LGA_SDA", "CASP12_LGA_LDDT"], y_vars=["vanilla", "Simple Arithmetic on partitioner 1", "Simple Arithmetic on partitioner 2", "Simple Arithmetic on partitioner 3"])
+    return f
 
 
 # Main; for callable scripts
@@ -40,7 +50,7 @@ def main():
     from argparse import ArgumentParser
     from sys import argv, stdin
     parser = ArgumentParser(
-        description="{one line to give a brief idea of what the program does.}")
+        description="Plot scatterplots from correlates in sqlite3 database")
     parser.add_argument(
         "-a", action="store_true", default=False, help="Prints nothing")
     parser.add_argument(
@@ -74,10 +84,7 @@ def main():
     correlates = d2S(correlates, "CASP12_LGA_SDA", d0)
 
     # Plot the correlations
-    (f, matrix) = plot_correlates(correlates, methods)
-
-    # Print correlation data to STDOUT
-    print(matrix)
+    f = plot_scatter(correlates)
 
     # Save figure
     f.savefig(outfile)
---
+++ plots.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+import numpy
+import pandas
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+'''
+ Plot correlation data in quality assessment sqlite3 database
+ Copyright (C) 2017  Robert Pilstål
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+def convert_data(correlates, methods):
+    # Format data into pandas frame
+    return pandas.DataFrame([entry[1:] for entry in correlates],
+                            columns=[method[1] for method in methods])
+
+
+def d2S(dataframe, method, d0):
+    dataframe[method] = dataframe[method].apply(lambda x: 1.0 / (1.0 + (x / d0)**2))
+    return dataframe
+
+
+def get_correlates(database):
+    query = "select id, name from method where type = 2 or type = 3;"
+    methods = database.execute(query).fetchall()
+
+    query = "select id from model;"
+    models = [entry[0] for entry in database.execute(query).fetchall()]
+
+    qa_query = "SELECT id FROM qa WHERE model = {} AND method = {} AND component IS NULL"
+    score_query = "SELECT residue, score FROM lscore WHERE qa = {}"
+
+    correlates = []
+    for model in models:
+        selects = []
+        froms = []
+        ons = []
+        previous = None
+        skip = False
+        # print("MODEL: {}".format(model))
+        for (num, (method_id, method_name)) in enumerate(methods):
+            tablename = "t{}".format(num)
+            qa = database.execute(qa_query.format(model, method_id)).fetchone()
+            if qa is not None:
+                # print("TABLE NUM: {}, METHOD ID: {}".format(num, method_id))
+                qa = qa[0]
+                if previous is None:
+                    selects.append("{}.residue".format(tablename))
+                selects.append("{}.score".format(tablename))
+                froms.append("({}) AS {}".format(score_query.format(qa), tablename))
+                if previous is not None:
+                    ons.append("{}.residue = {}.residue".format(previous, tablename))
+                previous = tablename
+            else:
+                # print("SKIPPING")
+                skip = True
+                break
+        if skip:
+            continue
+        current_query = "SELECT {} FROM {} ON {};".format(", ".join(selects), ", ".join(froms), " AND ".join(ons))
+        # print(current_query)
+        correlates += database.execute(current_query).fetchall()
+
+    return correlates, methods
+
+
+def plot_correlates(correlates, methods):
+    # seaborn setting
+    sns.set(style="white")
+
+    # Get correlation matrix via pandas
+    corrmatrix = correlates.corr()
+
+    # Generate a mask for the upper triangle
+    mask = numpy.zeros_like(corrmatrix, dtype=numpy.bool)
+    mask[numpy.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corrmatrix, mask=mask, cmap=cmap, vmax=.3, center=0,
+                    square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    return (f, corrmatrix)
